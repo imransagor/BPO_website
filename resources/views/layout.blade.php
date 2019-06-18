facf15b70b34a1484f384f6e941bbabd5fde0768
imransagor <imransagor22@yahoo.com>
2019-06-18 12:21:39 +0600

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,381 @@
+
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <title>Natural Multipurpose</title>
+    <!-- Meta tag Keywords -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+    <meta name="keywords"
+        content="Natural Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <script>
+        addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+    <!--// Meta tag Keywords -->
+
+    <!-- Custom-Files -->
+    <link rel="stylesheet" href="{{asset('frontend/css/bootstrap.css')}}">
+    <!-- Bootstrap-CSS -->
+    <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}" type="text/css" media="all" />
+    <!-- Style-CSS -->
+    <link href="{{asset('frontend/css/font-awesome.min.css')}}" rel="stylesheet">
+    <!-- Font-Awesome-Icons-CSS -->
+    <!-- //Custom-Files -->
+
+    <!-- Web-Fonts -->
+    <link
+        href="//fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese"
+        rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Tangerine:400,700" rel="stylesheet">
+    <!-- //Web-Fonts -->
+<!-- Social media link -->
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+ <!-- //Social media link -->
+
+</head>
+
+<body>
+    <!-- home -->
+    <div id="home">
+        <!-- banner -->
+        <div class="banner_w3lspvt">
+            <!-- nav -->
+            <div class="nav_w3ls pt-4 pb-3 text-center">
+                <nav>
+                    <label for="drop" class="toggle">Menu</label>
+                    <input type="checkbox" id="drop" />
+                    <ul class="menu">
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="about.html">About Us</a></li>
+                        <li><a href="#services">Services</a></li>
+                        <li><a href="#gallery">Gallery</a></li>
+                        <li><a href="contact.html">Contact Us</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <!-- //nav -->
+            <!-- banner slider -->
+            <div id="homepage-slider" class="st-slider">
+                <input type="radio" class="cs_anchor radio" name="slider" id="play1" checked="" />
+                <input type="radio" class="cs_anchor radio" name="slider" id="slide1" />
+                <input type="radio" class="cs_anchor radio" name="slider" id="slide2" />
+                <input type="radio" class="cs_anchor radio" name="slider" id="slide3" />
+                <div class="images">
+                    <div class="images-inner">
+                        <div class="image-slide">
+                            <div class="banner-w3ls-1">
+
+                            </div>
+                        </div>
+                        <div class="image-slide">
+                            <div class="banner-w3ls-2">
+
+                            </div>
+                        </div>
+                        <div class="image-slide">
+                            <div class="banner-w3ls-3">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="labels">
+                    <div class="fake-radio">
+                        <label for="slide1" class="radio-btn"></label>
+                        <label for="slide2" class="radio-btn"></label>
+                        <label for="slide3" class="radio-btn"></label>
+                    </div>
+                </div>
+                <!-- banner-text -->
+                <div class="banner-text">
+                    <fieldset>
+                        <legend>
+                            <a href="index.html" class="logo">Natural</a>
+                        </legend>
+                        <div class="banner-sub-txt">
+                            <h1 class="banner-txt">Nature is the art of God</h1>
+                            <p class="banner-txt1">Ut enim ad minim veniam, quis nostrud exercitation
+                                ullamco laboris
+                                nisi ut aliquip ex ea commodo consequat.</p>
+                            <a href="about.html" class="btn button-style mt-5">Read More</a>
+                        </div>
+                    </fieldset>
+                </div>
+                <!-- //banner-text -->
+            </div>
+            <!-- //banner slider -->
+        </div>
+        <!-- //banner -->
+    </div>
+    <!-- //home -->
+
+       <!-- testimonials -->
+    <section class="testimonials py-5 bg-li" id="testi">
+        <div class="container py-xl-4 py-lg-3">
+            <h3 class="tittle text-center">WHAT WE OFFER</h3>
+            <p class="sub-tittle text-center mt-3 mb-sm-5 mb-4">
+             quis nostrud exercitation</p>
+            <div class="row text-center mt-4">
+                <div class="col-lg-4 test-info">
+                    <p><span class="fa fa-quote-left mr-1"></span> Lorem Ipsum has been the industry's standard since
+                        the
+                        1500s. Praesent ullamcorper dui turpis.Nulla non laoreet eleifend. <span
+                            class="fa fa-quote-right ml-1"></span></p>
+                    <div class="test-img mb-3">
+                        <img src="images/te2.jpg" class="img-fluid" alt="user-image">
+                    </div>
+                    
+                </div>
+
+            </div>
+             <!-- social icons -->
+    <div class="d-flex justify-content-center row kpx_row-sm-offset-5 kpx_socialButtons">
+          <div class="col-xs-2 col-sm-2">
+            <a href="#" class="btn btn-lg btn-block kpx_btn-facebook" data-toggle="tooltip" data-placement="top" title="Facebook">
+              <i class="fa fa-facebook fa-2x"></i>
+              <span class="hidden-xs"></span>
+            </a>
+          </div>
+          <div class="col-xs-2 col-sm-2">
+            <a href="#" class="btn btn-lg btn-block kpx_btn-twitter" data-toggle="tooltip" data-placement="top" title="Twitter">
+              <i class="fa fa-twitter fa-2x"></i>
+              <span class="hidden-xs"></span>
+            </a>
+          </div>  
+          <div class="col-xs-2 col-sm-2">
+            <a href="#" class="btn btn-lg btn-block kpx_btn-google-plus" data-toggle="tooltip" data-placement="top" title="Google Plus">
+              <i class="fa fa-google-plus fa-2x"></i>
+              <span class="hidden-xs"></span>
+            </a>
+          </div> 
+          <div class="col-xs-2 col-sm-2">
+            <a href="#" class="btn btn-lg btn-block kpx_btn-instagram" data-toggle="tooltip" data-placement="top" title="instagram">
+              <i class="fa fa-instagram fa-2x"></i>
+              <span class="hidden-xs"></span>
+            </a>
+          </div>
+          <div class="col-xs-2 col-sm-2">
+            <a href="#" class="btn btn-lg btn-block kpx_btn-youtube" data-toggle="tooltip" data-placement="top" title="YouTube">
+              <i class="fa fa-youtube fa-2x"></i>
+              <span class="hidden-xs"></span>
+            </a>
+          </div> 
+    </div>
+         
+            <!-- //social icons -->
+
+        </div>
+    </section>
+    <!-- //testimonials -->
+
+     <!-- services -->
+    <section class="services text-center py-5" id="services">
+        <div class="container">
+            <h3 class="tittle text-center">Our Services</h3>
+            <p class="sub-tittle text-center mt-3 mb-sm-5 mb-4">Sed do eiusmod tempor incididunt ut labore et dolore
+                magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+            <div class="row ser-sec-1">
+                <div class="col-md-4 ser-w3ls-wthree">
+                    <div class="icon">
+                        <span class="fa fa-lightbulb-o"></span>
+                    </div>
+                    <!-- Icon ends here -->
+                    <div class="service-content">
+                        <h5>Service 01</h5>
+                        <p class="serp">
+                            Quisque sagittis lacus eu lorem sodalesd enean adipiscing.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- //services -->
+
+
+    <!-- about -->
+    <section class="testimonials py-2 bg-li">
+        <div class="py-xl-3 py-lg-2">
+            <h3 class="tittle text-center">Partnered to and recommended by the best</h3>
+            <div class="d-xl-flex about-grids">
+                <div class="col-xl-2 about-left p-4">
+                    <img src="{{asset('frontend/images/ab3.jpg')}}" alt="" class="img-fluid" />
+                </div>
+                <div class="col-xl-2 about-left p-4">
+                    <img src="{{asset('frontend/images/ab3.jpg')}}" alt="" class="img-fluid" />
+                </div>
+                <div class="col-xl-4 about-right border-bottom">
+                    
+                    <p class="mb-3">Integer pulvinar leo id viverra feugiat. Pellentesque libero ut justo.</p>
+                    <p>Semper at tempus vel, ultrices in ligula. </p>
+                    <a href="#" class="btn button-style-3">Learn More</a>
+                </div>
+            </div>
+            
+        </div>
+    </section>
+    <!-- //about -->
+
+   
+  
+    <!-- contact -->
+    <section class="contact py-5" id="contact">
+        <div class="container py-md-5">
+            <h3 class="tittle text-center">Contact Us</h3>
+            <p class="sub-tittle text-center mt-3 mb-sm-5 mb-4">Sed do eiusmod tempor incididunt ut labore et
+                dolore
+                magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+            <div class="row mt-4">
+                <div class="col-lg-8 mb-5">
+                    <form action="#" method="post">
+                        <div class="row main-w3layouts-sectns">
+                            <div class="col-md-6 w3-btm-spc form-text1">
+                                <input type="text" name="Name" placeholder="Enter Your Name" required="">
+                            </div>
+                            <div class="col-md-6 w3-btm-spc form-text2">
+                                <input type="text" name="Phone no" placeholder="Enter Phone Number" required="">
+                            </div>
+                        </div>
+                        <div class="row main-w3layouts-sectns">
+                            <div class="col-md-6 w3-btm-spc form-text1">
+                                <input type="email" name="email" placeholder="Enter Your Email" required="">
+                            </div>
+                            <div class="col-md-6 w3-btm-spc form-text2">
+                                <input type="text" name="subject" placeholder="Subject" required="">
+                            </div>
+                        </div>
+                        <div class="main-w3layouts-sectns ">
+                            <div class="w3-btm-spc form-text2 p-0">
+                                <textarea placeholder="Enter Your Message Here" name="message"></textarea>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn mt-3">Submit</button>
+                    </form>
+                </div>
+                <div class="col-lg-4 mb-5">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d158857.728106568!2d-0.24168153701090248!3d51.52877184090542!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondon%2C+UK!5e0!3m2!1sen!2sin!4v1544074523717"></iframe>
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col-lg-3 contact-left">
+                    <span>Dont Wait</span>
+                    <h3 class="heading mb-lg-5 mb-4">Send Message</h3>
+                </div>
+                <div class="col-lg-9 contact-text-bot">
+                    <p>If you have any queries, or want to speak to one of our customer support
+                        executive, Please contact us for more details by below given information.</p>
+                    <div class="row mt-4">
+                        <div class="contact-info col-md-6 pr-md-5">
+                            <h4 class="mb-3">Address 01</h4>
+                            <div class="footer-style-w3ls">
+                                <p><strong>Location</strong> : Customer Agency, 5th cross, 4th building, New York City.
+                                </p>
+                            </div>
+                            <div class="footer-style-w3ls">
+                                <p><strong>Phone</strong> : +121 098 8907 9987</p>
+                            </div>
+                            <div class="footer-style-w3ls">
+                                <p><strong>Fax</strong> : +121 098 8907 9987</p>
+                            </div>
+                            <div class="footer-style-w3ls">
+                                <p><strong>Email</strong> : <a href="mailto:info@example.com">info@example.com</a></p>
+                            </div>
+                        </div>
+                        <div class="contact-info col-md-6 pr-md-5 mt-md-0 mt-4">
+                            <h4 class="mb-3">Address 02</h4>
+                            <div class="footer-style-w3ls">
+                                <p><strong>Location</strong> : Customer Agency, 2nd cross, 4th floor, London.</p>
+                            </div>
+                            <div class="footer-style-w3ls">
+                                <p><strong>Phone</strong> : +121 098 8907 9987</p>
+                            </div>
+                            <div class="footer-style-w3ls">
+                                <p><strong>Fax</strong> : +121 098 8907 9987</p>
+                            </div>
+                            <div class="footer-style-w3ls">
+                                <p><strong>Email</strong> : <a href="mailto:info@example.com">info@example.com</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- //contact -->
+
+    <!-- footer -->
+    <footer class="footer-sec-w3layouts py-5 ">
+        <div class="container py-xl-5 py-lg-3">
+            <div class="row pb-3">
+                <div class="col-xl-5 col-md-6 footer-left-info text-left">
+                    <div class="logo-footer">
+                        <h2>
+                            <a href="index.html">Natural</a>
+                        </h2>
+                    </div>
+                   <p>contact form</p>
+                </div>
+                <div class="col-md-6 footer-right-info text-right offset-xl-1 mt-md-0 mt-5">
+                   <p>Testimonials.some comments from clients.
+                   </p>
+                </div>
+            </div>
+            <!-- social icons -->
+            <div class="social-icons-footer social-w3pvt-footer border-top text-center pt-md-5 pt-4 mt-md-5 mt-4">
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#">
+                            <span class="fa fa-facebook"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="fa fa-twitter"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="fa fa-dribbble"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="fa fa-instagram"></span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <!-- //social icons -->
+        </div>
+    </footer>
+    <!-- //footer -->
+    <!-- copyright -->
+    <div class="cpy-right text-center py-3">
+        <p>All rights reserved | Design by
+            <a href="#"> .</a>
+        </p>
+    </div>
+    <!-- //copyright -->
+    <!-- move top icon -->
+    <a href="#home" class="move-top text-center">
+        <span class="fa fa-chevron-circle-up" aria-hidden="true"></span>
+    </a>
+    <!-- //move top icon -->
+
+
+</body>
+
+</html>
